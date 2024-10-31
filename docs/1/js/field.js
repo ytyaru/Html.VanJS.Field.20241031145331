4e9972e2c87859ed75517ed96a2849e62f4872e3
ytyaru <yttry0@gmail.com>
2024-10-31 16:08:30 +0900

    add 1 途中

---
+++ docs/1/js/field.js
@@ -0,0 +1,54 @@
+;(function(){
+class Field {
+    #_; #v;
+    static of(...args) {return new Field(...args)}
+    static #NAMES = 'Boolean,Integer,Bigint,Float,String,Symbol,Function,AsyncFunction,GeneratorFunction,AsyncGeneratorFunction,Error,Class,Instance,Promise,Iterator,Object,Array,Map,Set,WeakMap,WeakSet,Proxy,Date,Regexp,URL,Element,Int8Array,Uint8Array,Uint8ClampedArray,Int16Array,Uint16Array,Int32Array,Uint32Array,BigInt64Array,BigUint64Array,Float16Array,Float32Array,Float64Array,ArrayBuffer,SharedArrayBuffer,DataView'.split(',')
+    static get NAMES() {return this.#NAMES }
+    constructor(name, type, value, mutable=false, getter=null, setter=null) {
+        this.#valid(name, type, value, mutable, getter, setter)
+        this.#_ = Object.freeze({name:name, type:type, value:value, mutable:mutable})
+        this.#v = value
+        return new Proxy(this, {
+            get(t,k){
+                if ('n,t,v'.split(',').some(K=>K===k)) {return t[k]}
+                throw new TypeError(`'${k}' is invalid property name. valid name is 'n','t','v','m'.`)
+            },
+        })
+//        return Object.freeze(this)
+        /*
+        Object.defineProperty(this, '_', {
+            value: Object.freeze({name:name, type:type, value:value, mutable:mutable}),
+            enumerable:false,
+            configurable:false,
+            writable:false,
+        })
+        this._value = value
+        //Object.defineProperty(this, '_value', {value:value, enumerable:false, writable:true})
+        //Object.defineProperty(this.constructor.prototype, 'v', {enumerable:true});
+        //Object.defineProperty(Field.prototype, 'v', {enumerable:true});
+        */
+    }
+    get n() { return this.#_.name }
+    get t() { return this.#_.type }
+    get v() { return this.#v }
+    set v(v) {
+        if (this.#_.mutable) {
+            if (Type[`is${this.#_.type}`](v)) {this.#v = v}
+            else {throw new TypeError(`Invalid type.`)}
+        } else {throw new TypeError(`'${this.#_.name}' is immutable.`)}
+    }
+    get m() { return this.#_.mutable }
+    #valid(name, type, value, mutable=false, getter=null, setter=null) {
+        if (!this.#validName(name)){throw new TypeError(`Invalid name.`)}
+        if (!this.#validType(type)){throw new TypeError(`Invalid type.`)}
+        if (!this.#validValue(value,type)){throw new TypeError(`Invalid value.`)}
+    }
+    #validName(name) { return /^[_a-zA-Z][_a-zA-Z0-9]*$/.test(name) }
+    #validType(type) { return Field.#NAMES.includes(type) }
+    #validValue(value, type) { return Type.isCls(type) ? value instanceof type : Type[`is${type}`](value) }
+    #validGetter(getter, type) {}
+    #validSetter(getter, type) {}
+}
+//window.Field = Field
+window.Field = Object.freeze(Field)
+})();
