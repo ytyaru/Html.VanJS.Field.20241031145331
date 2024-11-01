eb37a6d72cfa25a03ebec73b1418c2af019cba40
ytyaru <yttry0@gmail.com>
2024-11-01 20:04:22 +0900

    add 3 途中

---
+++ docs/3/js/main.js
@@ -0,0 +1,113 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru'
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/Html.VanJS.Field.20241031145331/`}, 'Field')),
+        van.tags.p('Fieldを作成する。'),
+//        p('Create a field.'),
+    )
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+
+    const a = new Assertion()
+    const bb = new BlackBox(a)
+    a.t(Type.isCls(Field))
+    a.t(Type.isStrs(Field.NAMES))
+    ;(function(){
+        const name = new Field('name', 'String')
+        a.t('n' in name)
+        a.t('t' in name)
+        a.t('v' in name)
+        a.e(TypeError, `Read only.`, ()=>name.n = 'x') // 名は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.t = 'x') // 型は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.x = 'x') // 新しいプロパティを追加することはできない。
+        a.f('x' in name) 
+        a.t('name'===name.n)
+        a.t('String'===name.t)
+        console.log(name.v)
+        a.t(''===name.v)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = 'Suzuki') // 値は変更不可(mutable:falseのため)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = null) // 値は変更不可(mutable:falseのため)
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', 'Yamada')
+        a.t('n' in name)
+        a.t('t' in name)
+        a.t('v' in name)
+        a.e(TypeError, `Read only.`, ()=>name.n = 'x') // 名は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.t = 'x') // 型は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.x = 'x') // 新しいプロパティを追加することはできない。
+        a.f('x' in name) 
+        a.t('name'===name.n)
+        a.t('String'===name.t)
+        a.t('Yamada'===name.v)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = 'Suzuki') // 値は変更不可(mutable:falseのため)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = null) // 値は変更不可(mutable:falseのため)
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', 'Yamada', true) // nullable
+        a.t('n' in name)
+        a.t('t' in name)
+        a.t('v' in name)
+        a.e(TypeError, `Read only.`, ()=>name.n = 'x') // 名は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.t = 'x') // 型は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.x = 'x') // 新しいプロパティを追加することはできない。
+        a.f('x' in name) 
+        a.t('name'===name.n)
+        a.t('String'===name.t)
+        a.t('Yamada'===name.v)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = 'Suzuki') // 値は変更不可(mutable:falseのため)
+        a.e(TypeError, `'name' is immutable.`, ()=>name.v = null) // 値は変更不可(mutable:falseのため)
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', 'Yamada', false, true) // mutable
+        a.t('n' in name)
+        a.t('t' in name)
+        a.t('v' in name)
+        a.e(TypeError, `Read only.`, ()=>name.n = 'x') // 名は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.t = 'x') // 型は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.x = 'x') // 新しいプロパティを追加することはできない。
+        a.f('x' in name) 
+        a.t('name'===name.n)
+        a.t('String'===name.t)
+        a.t('Yamada'===name.v)
+        name.v = 'Suzuki'
+        a.t('Suzuki'===name.v) // 値は変更可(mutable:trueのため)
+        a.e(TypeError, `'name' is not nullable.`, ()=>name.v = null) // 値は変更不可(mutable:falseのため)
+        a.t('Suzuki'===name.v) // 値は変更可(mutable:trueのため)
+    })();
+    ;(function(){
+         // nullable:falseなのにnullをセットしようとした
+        a.e(TypeError, `'name' is not nullable.`, ()=>new Field('name', 'String', null))
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', '', false, true) // nullable:falseなのにnullをセットしようとした
+        a.e(TypeError, `'name' is not nullable.`, ()=>name.v = null)
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', null, true)
+        a.t(null===name.v)
+    })();
+    ;(function(){
+        const name = new Field('name', 'String', null, true, true) // nullable, mutable
+        a.t('n' in name)
+        a.t('t' in name)
+        a.t('v' in name)
+        a.e(TypeError, `Read only.`, ()=>name.n = 'x') // 名は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.t = 'x') // 型は変更不可
+        a.e(TypeError, `Read only.`, ()=>name.x = 'x') // 新しいプロパティを追加することはできない。
+        a.f('x' in name) 
+        a.t('name'===name.n)
+        a.t('String'===name.t)
+        a.t(null===name.v)
+        name.v = 'Suzuki'
+        a.t('Suzuki'===name.v) // 値は変更可(mutable:trueのため)
+        name.v = null
+        a.t(null===name.v) // 値は変更可(mutable:trueのため)
+    })();
+
+    a.fin()
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
